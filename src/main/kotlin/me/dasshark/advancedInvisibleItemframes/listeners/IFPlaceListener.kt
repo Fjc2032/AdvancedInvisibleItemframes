b621102d93f10fa5d824b4c9964e5b10dad1d787
Fjc <155930341+Fjc2032@users.noreply.github.com>
2025-04-03 15:50:47 +0000

    Update IFPlaceListener.kt

--- src/main/kotlin/me/dasshark/advancedInvisibleItemframes/listeners/IFPlaceListener.kt
+++ src/main/kotlin/me/dasshark/advancedInvisibleItemframes/listeners/IFPlaceListener.kt
@@ -3,6 +3,7 @@ package me.dasshark.advancedInvisibleItemframes.listeners
 import me.dasshark.advancedInvisibleItemframes.Main
 import org.bukkit.Material
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.GlowItemFrame
 import org.bukkit.entity.ItemFrame
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -14,23 +15,42 @@ import org.bukkit.persistence.PersistentDataType
 class IFPlaceListener: Listener {
 
     @EventHandler
-    fun onHangingPlace(e: HangingPlaceEvent) {
-        if (e.entity.type != EntityType.ITEM_FRAME) return
+    fun onHangingPlace(event: HangingPlaceEvent) {
+        if (event.entity.type != EntityType.ITEM_FRAME) return
         val frame: ItemStack
-        val p = e.player ?: return
+        val player = event.player ?: return
 
-        if (p.inventory.itemInMainHand.type === Material.ITEM_FRAME)
-            frame = p.inventory.itemInMainHand
-        else if (p.inventory.itemInOffHand.type === Material.ITEM_FRAME)
-            frame = p.inventory.itemInOffHand
+        if (player.inventory.itemInMainHand.type === Material.ITEM_FRAME)
+            frame = player.inventory.itemInMainHand
+        else if (player.inventory.itemInOffHand.type === Material.ITEM_FRAME)
+            frame = player.inventory.itemInOffHand
         else
             return
 
         if (frame.itemMeta!!.persistentDataContainer.has(Main.invisibleKey, PersistentDataType.BYTE)) {
-            val itemFrame: ItemFrame = e.entity as ItemFrame
+            val itemFrame: ItemFrame = event.entity as ItemFrame
             itemFrame.isVisible = false
-            e.entity.persistentDataContainer.set(Main.invisibleKey, PersistentDataType.BYTE, 1)
+            event.entity.persistentDataContainer.set(Main.invisibleKey, PersistentDataType.BYTE, 1)
         }
     }
+    @EventHandler
+    fun onGlowHangingPlace(event: HangingPlaceEvent) {
+        if (event.entity.type !== EntityType.GLOW_ITEM_FRAME) return
+        val glowFrame: ItemStack
+        val player = event.player ?: return
+
+        if (player.inventory.itemInMainHand.type === Material.GLOW_ITEM_FRAME)
+            glowFrame = player.inventory.itemInMainHand
+        else if (player.inventory.itemInOffHand.type === Material.GLOW_ITEM_FRAME)
+            glowFrame = player.inventory.itemInOffHand
+        else return
+
+        if (glowFrame.itemMeta!!.persistentDataContainer.has(Main.invisibleKey, PersistentDataType.BYTE)) {
+            val glowItemFrame: GlowItemFrame = event.entity as GlowItemFrame
+            glowItemFrame.isVisible = false
+            event.entity.persistentDataContainer.set(Main.invisibleKey, PersistentDataType.BYTE, 1)
+        }
+    }
+
 
 }
