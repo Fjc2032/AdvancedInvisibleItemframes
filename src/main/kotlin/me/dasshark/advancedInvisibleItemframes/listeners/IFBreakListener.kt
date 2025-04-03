661c95a23ab8768c8fc58a7e9c1e04270605ca7c
Fjc <155930341+Fjc2032@users.noreply.github.com>
2025-04-03 15:50:26 +0000

    Update IFBreakListener.kt

--- src/main/kotlin/me/dasshark/advancedInvisibleItemframes/listeners/IFBreakListener.kt
+++ src/main/kotlin/me/dasshark/advancedInvisibleItemframes/listeners/IFBreakListener.kt
@@ -1,6 +1,8 @@
 package me.dasshark.advancedInvisibleItemframes.listeners
 
 import me.dasshark.advancedInvisibleItemframes.Main
+import org.bukkit.Material
+import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.hanging.HangingBreakEvent
@@ -13,8 +15,14 @@ class IFBreakListener : Listener {
      fun onHangingBreak(event: HangingBreakEvent) {
          if (event.entity.persistentDataContainer.has(Main.invisibleKey, PersistentDataType.BYTE)) {
              event.isCancelled = true
-             event.entity.remove()
-             event.entity.world.dropItemNaturally(event.entity.location, Main.instance.getInvisFrame(false)!!)
+             if (event.entity.type == EntityType.ITEM_FRAME) {
+                 event.entity.remove()
+                 event.entity.world.dropItemNaturally(event.entity.location, Main.instance.getInvisFrame(false)!!)
+             }
+             else if (event.entity.type == EntityType.GLOW_ITEM_FRAME) {
+                 event.entity.remove()
+                 event.entity.world.dropItemNaturally(event.entity.location, Main.instance.getGlowInvisFrame(false)!!)
+             }
          }
      }
 
